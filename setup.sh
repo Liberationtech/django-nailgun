0918541804bed15b7151e5d86f15975047f40d62
Oivvio Polite <oivvio@liberationtech.net>
2013-01-01 21:24:49 +0100

    tat

---
+++ setup.sh
@@ -0,0 +1,7 @@
+. $NAILGUNSETTINGS
+
+#XXX TODO this should all be escaped so that they could be overridden from $NAILGUNSETTINGS
+PROJECTMODULE=$WORKSPACE/$PROJECTNAME
+settings=$PROJECTMODULE/$PROJECTNAME/settings.py
+local_settings_localhost=$PROJECTMODULE/$PROJECTNAME/local_settings_$(hostname).py
+local_settings=$PROJECTMODULE/$PROJECTNAME/local_settings.py
